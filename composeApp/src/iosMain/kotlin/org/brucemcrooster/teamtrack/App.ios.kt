102124433e46cdf9510fa1139cd01b7702c87cac
Evan <53529192+BruceMcRooster@users.noreply.github.com>
2023-08-09 08:52:59 -0400

    Generated new project using [Compose Multiplatform Wizard](https://terrakok.github.io/Compose-Multiplatform-Wizard/)
    I changed the password show/hide button to FeatherIcon.Eye/EyeOff because it was annoying me

---
+++ composeApp/src/iosMain/kotlin/org/brucemcrooster/teamtrack/App.ios.kt
@@ -0,0 +1,9 @@
+package org.brucemcrooster.teamtrack
+
+import platform.Foundation.NSURL
+import platform.UIKit.UIApplication
+
+internal actual fun openUrl(url: String?) {
+    val nsUrl = url?.let { NSURL.URLWithString(it) } ?: return
+    UIApplication.sharedApplication.openURL(nsUrl)
+}
